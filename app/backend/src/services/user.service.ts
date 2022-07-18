774e3769f6954b9c20746c33e944711bcecfa3d1
marllomartin <marllon3636@gmail.com>
2022-07-18 19:01:59 -0300

    feat(services/user.service): Create Login validation service - loginValidate

--- app/backend/src/services/user.service.ts
+++ app/backend/src/services/user.service.ts
@@ -1,4 +1,4 @@
-import { sign } from 'jsonwebtoken';
+import { sign, verify } from 'jsonwebtoken';
 import 'dotenv/config';
 import Login from '../interfaces/login.interface';
 import UserModel from '../database/models/user.model';
@@ -9,17 +9,26 @@ const { JWT_SECRET } = process.env;
 class UserService {
   static async login(login: Login): Promise<object> {
     const { email, password } = login;
-    const verify = await UserModel.findOne({ where: { email } });
+    const find = await UserModel.findOne({ where: { email } });
 
-    if (!verify) throw new Error('Incorrect email or password');
-    if (verify) {
-      const auth = await authPassword(password, verify?.password);
+    if (!find) throw new Error('Incorrect email or password');
+    if (find) {
+      const auth = await authPassword(password, find?.password);
       if (!auth) throw new Error('Incorrect email or password');
     }
     const secret = String(JWT_SECRET);
     const token = sign({ data: email }, secret, { expiresIn: '1h' });
 
     return { token };
+  }
+
+  static async loginValidate(token: string | undefined) {
+    const secret = String(JWT_SECRET);
+    const decode = verify(token as string, secret);
+    const { data } = decode as { data: string };
+    const find = await UserModel.findOne({ where: { email: data } });
+
+    return { role: find?.role };
   }
 }
 
