ab3f860bb05d27e4bad7a8b53192716aa71e1239
marllomartin <marllon3636@gmail.com>
2022-07-20 19:00:08 -0300

    feat(controller/matches.controller):Adiciona função de filtrar Matches no getAll

--- app/backend/src/controllers/matches.controller.ts
+++ app/backend/src/controllers/matches.controller.ts
@@ -4,6 +4,17 @@ import getErrorMessage from '../utils/getErrorMessage';
 
 class MatchesController {
   static getAll = async (req: Request, res: Response) => {
+    const { inProgress } = req.query;
+
+    if (inProgress) {
+      try {
+        const result = await MatchesService.getAllFiltered(inProgress === 'true');
+        return res.status(200).json(result);
+      } catch (Error) {
+        return res.status(404).send({ message: getErrorMessage(Error) });
+      }
+    }
+
     try {
       const result = await MatchesService.getAll();
       res.status(200).json(result);
