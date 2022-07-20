9a5bc4a79718ac70fc05b8afb0eacd6e12c7636d
marllomartin <marllon3636@gmail.com>
2022-07-20 18:01:28 -0300

    feat(services/matches.service): Cria service de Matches - getAll

---
+++ app/backend/src/services/matches.service.ts
@@ -0,0 +1,19 @@
+import TeamModel from '../database/models/team.model';
+import MatchModel from '../database/models/match.model';
+
+class MatchesService {
+  static async getAll(): Promise<object> {
+    const matches = await MatchModel.findAll({
+      include: [
+        { model: TeamModel, as: 'teamHome', attributes: ['teamName'] },
+        { model: TeamModel, as: 'teamAway', attributes: ['teamName'] },
+      ],
+    });
+
+    if (!matches) throw new Error('No matches found');
+
+    return matches;
+  }
+}
+
+export default MatchesService;
