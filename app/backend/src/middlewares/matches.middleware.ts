6e9e7a21d1701db089f881488eacd4909df9afc4
marllomartin <marllon3636@gmail.com>
2022-07-21 13:41:09 -0300

    fix(middlewares/matches.middleware): Ajusta status http

--- app/backend/src/middlewares/matches.middleware.ts
+++ app/backend/src/middlewares/matches.middleware.ts
@@ -11,7 +11,7 @@ class MatchValidation {
     const { homeTeam, awayTeam } = req.body;
 
     if (homeTeam === awayTeam) {
-      return res.status(400)
+      return res.status(401)
         .json({ message: 'It is not possible to create a match with two equal teams' });
     }
 
