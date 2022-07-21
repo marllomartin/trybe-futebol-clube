4d8f51e5150f9862566ac70c8a870b5f52987752
marllomartin <marllon3636@gmail.com>
2022-07-21 12:17:53 -0300

    feat(routes/matches.routes): Cria novas rotas de Matches

--- app/backend/src/routes/matches.routes.ts
+++ app/backend/src/routes/matches.routes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/matches.controller';
+import MatchValidation from '../middlewares/matches.middleware';
+
+const { checkEqualTeams, checkExistingTeam, authToken } = MatchValidation;
 
 const router = Router();
 
 // Matches
 router.get('/matches', MatchesController.getAll);
+router.post('/matches', checkEqualTeams, checkExistingTeam, authToken, MatchesController.create);
+router.patch('/matches/:id/finish', MatchesController.finish);
 
 export default router;
