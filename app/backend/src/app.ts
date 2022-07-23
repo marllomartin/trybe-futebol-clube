21d3a20fd672ca1da844d04b2278bd9796f9c980
marllomartin <marllon3636@gmail.com>
2022-07-23 19:55:35 -0300

    feat(src/app): Add Leaderboard routes to app

--- app/backend/src/app.ts
+++ app/backend/src/app.ts
@@ -2,6 +2,7 @@ import * as express from 'express';
 import UserRoutes from './routes/user.routes';
 import TeamsRoutes from './routes/teams.routes';
 import MatchesRoutes from './routes/matches.routes';
+import LeaderboardRoutes from './routes/leaderboard.routes';
 
 class App {
   public app: express.Express;
@@ -26,7 +27,7 @@ class App {
     this.app.use(express.json());
     this.app.use(accessControl);
 
-    this.app.use(UserRoutes, TeamsRoutes, MatchesRoutes);
+    this.app.use(UserRoutes, TeamsRoutes, MatchesRoutes, LeaderboardRoutes);
   }
 
   public start(PORT: string | number):void {
