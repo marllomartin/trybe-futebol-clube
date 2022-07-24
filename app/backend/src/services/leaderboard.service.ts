892d0649580cf4392d37ddcbfceff0686c5b95c8
marllomartin <marllon3636@gmail.com>
2022-07-23 22:47:29 -0300

    feat(services/leaderboard.service): Adjust Leaderboard service - update getLeaderboardHome - add getLeaderBoardAway

--- app/backend/src/services/leaderboard.service.ts
+++ app/backend/src/services/leaderboard.service.ts
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 import TeamModel from '../database/models/team.model';
 import MatchModel from '../database/models/match.model';
 import leaderboardFunctions from '../utils/leaderboardFunctions';
@@ -25,15 +26,39 @@ class LeaderboardService {
 
       return {
         name: team.teamName,
-        totalPoints: calcTotalPoints(matches),
+        totalPoints: calcTotalPoints(matches, 'h'),
         totalGames: calcTotalGames(matches),
-        totalVictories: calcTotalVictories(matches),
+        totalVictories: calcTotalVictories(matches, 'h'),
         totalDraws: calcTotalDraws(matches),
-        totalLosses: calcTotalLosses(matches),
-        goalsFavor: calcGoalsFavor(matches),
-        goalsOwn: calcGoalsOwn(matches),
-        goalsBalance: calcGoalsBalance(calcGoalsFavor(matches), calcGoalsOwn(matches)),
-        efficiency: calcEfficiency(calcTotalPoints(matches), calcTotalGames(matches)),
+        totalLosses: calcTotalLosses(matches, 'h'),
+        goalsFavor: calcGoalsFavor(matches, 'h'),
+        goalsOwn: calcGoalsOwn(matches, 'h'),
+        goalsBalance: calcGoalsBalance(calcGoalsFavor(matches, 'h'), calcGoalsOwn(matches, 'h')),
+        efficiency: calcEfficiency(calcTotalPoints(matches, 'h'), calcTotalGames(matches)),
+      };
+    });
+
+    return sortLeaderboard(leaderboards);
+  }
+
+  static async getLeaderboardAway(): Promise<Leaderboard[]> {
+    const allMatches = await MatchModel.findAll({ where: { inProgress: 0 } });
+    const allTeams = await TeamModel.findAll();
+
+    const leaderboards = allTeams.map((team) => {
+      const matches = allMatches.filter((match) => match.awayTeam === team.id);
+
+      return {
+        name: team.teamName,
+        totalPoints: calcTotalPoints(matches, 'a'),
+        totalGames: calcTotalGames(matches),
+        totalVictories: calcTotalVictories(matches, 'a'),
+        totalDraws: calcTotalDraws(matches),
+        totalLosses: calcTotalLosses(matches, 'a'),
+        goalsFavor: calcGoalsFavor(matches, 'a'),
+        goalsOwn: calcGoalsOwn(matches, 'a'),
+        goalsBalance: calcGoalsBalance(calcGoalsFavor(matches, 'a'), calcGoalsOwn(matches, 'awayTeam')),
+        efficiency: calcEfficiency(calcTotalPoints(matches, 'a'), calcTotalGames(matches)),
       };
     });
 
