2488d7f1debc0bd526ca5ab57edc832f57794988
marllomartin <marllon3636@gmail.com>
2022-07-24 14:53:36 -0300

    test(tests/leaderboard.test): Implementa testes da rota /leaderboard

---
+++ app/backend/src/tests/leaderboard.test.ts
@@ -0,0 +1,60 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+// @ts-ignore
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+describe('[GET] Leaderboard Home', () => {
+  it('Status 200 is returned get leaderboard home request is successful', async () => {
+    const res = await chai.request(app).get('/leaderboard/home');
+
+    expect(res.status).to.be.equal(200);
+  });
+  it('Leaderboard Home is returned when get leaderboard home request is successful', async () => {
+    const res = await chai.request(app).get('/leaderboard/home');
+
+    expect(res.body).to.be.an('Array');
+    expect(res.body[0]).to.have.keys(
+      'name',
+      'totalPoints',
+      'totalGames',
+      'totalVictories',
+      'totalDraws',
+      'totalLosses',
+      'goalsFavor',
+      'goalsOwn',
+      'goalsBalance',
+      'efficiency'
+    );
+  });
+});
+
+describe('[GET] Leaderboard Away', () => {
+  it('Status 200 is returned get leaderboard away request is successful', async () => {
+    const res = await chai.request(app).get('/leaderboard/away');
+
+    expect(res.status).to.be.equal(200);
+  });
+  it('Leaderboard Away is returned when get leaderboard away request is successful', async () => {
+    const res = await chai.request(app).get('/leaderboard/away');
+
+    expect(res.body).to.be.an('Array');
+    expect(res.body[0]).to.have.keys(
+      'name',
+      'totalPoints',
+      'totalGames',
+      'totalVictories',
+      'totalDraws',
+      'totalLosses',
+      'goalsFavor',
+      'goalsOwn',
+      'goalsBalance',
+      'efficiency'
+    );
+  });
+});
