b1980a8f8dddf96a4535b976a9054e13ee8ef4fb
marllomartin <marllon3636@gmail.com>
2022-07-23 22:49:10 -0300

    feat(routes/leaderboard.routes): Update leaderboard routes

--- app/backend/src/routes/leaderboard.routes.ts
+++ app/backend/src/routes/leaderboard.routes.ts
@@ -4,6 +4,7 @@ import LeaderboardController from '../controllers/leaderboard.controller';
 const router = Router();
 
 // Leaderboards
-router.get('/leaderboard/home', LeaderboardController.getLeaderBoardHome);
+router.get('/leaderboard/home', LeaderboardController.getLeaderboardHome);
+router.get('/leaderboard/away', LeaderboardController.getLeaderboardAway);
 
 export default router;
